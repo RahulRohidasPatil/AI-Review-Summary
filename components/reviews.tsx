785db5e30701b600f33df4a30cfd58318835b5cd
RahulPatil <rahulrohidaspatil@gmail.com>
2026-02-13 00:08:37 +0100

    Exercise 1.3

---
+++ components/reviews.tsx
@@ -0,0 +1,22 @@
+import type { Product } from "@/lib/types";
+import { Review } from "./review";
+import { Separator } from "./ui/separator";
+
+export function Reviews({ product }: { product: Product }) {
+  return (
+    <div className="space-y-6">
+      <h2 className="text-2xl font-bold">Customer Reviews</h2>
+
+      <div className="space-y-6">
+        {product.reviews.map((review, index) => (
+          <div key={review.date}>
+            <Review review={review} />
+            {index < product.reviews.length - 1 && (
+              <Separator className="mt-6" />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
